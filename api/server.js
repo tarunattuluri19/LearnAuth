f140a920476783b00e53b8cfe426132cc0825b4f
tarun <tarunlearning27@gmail.com>
2024-11-08 15:14:02 +0530

    commiting

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express=require('express') 
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
-dotenv.config()
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoute from './routes/user.route.js'
+dotenv.config();
 const app=express()
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
@@ -10,6 +11,15 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log(err)
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+app.listen(5000,()=>{
+    console.log("Server is running on port on 5000")
+})
+
+app.use("/api/user", userRoute)
+
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:"Welcome to Auth API"
+    })
 })
