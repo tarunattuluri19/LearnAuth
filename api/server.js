4b94d0d38948d3331a3b43f8a7311fd83407198a
tarun <tarunlearning27@gmail.com>
2024-11-08 13:25:46 +0530

    second commit

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express=require('express') 
 const mongoose=require('mongoose')
-
+const dotenv=require('dotenv')
+dotenv.config()
 const app=express()
 
-mongoose.connect(" ")
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connected to db")
+}).catch((err)=>{
+    console.log(err)
+})
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
