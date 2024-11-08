557655c301eb86343c2eec334d9f45f8a651071b
tarun <tarunlearning27@gmail.com>
2024-11-08 17:03:05 +0530

    middleware done

--- api/server.js
+++ api/server.js
@@ -27,3 +27,13 @@ app.get("/", (req, res) => {
     message: "Welcome to Auth API",
   });
 });
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal server error'
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+    })
+})
