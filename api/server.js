1701b4424394ecf06170fc26010c873779279195
tarun <tarunlearning27@gmail.com>
2024-11-08 13:06:56 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express=require('express') 
+const mongoose=require('mongoose')
+
+const app=express()
+
+mongoose.connect(" ")
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
