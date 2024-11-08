651806ca64ef645e07b740f304e6ab9e23fd0cb0
tarun <tarunlearning27@gmail.com>
2024-11-08 16:14:56 +0530

    signup done in api

--- api/server.js
+++ api/server.js
@@ -1,25 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 dotenv.config();
-const app=express()
+const app = express();
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("Connected to db")
-}).catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json());
+app.listen(5000, () => {
+  console.log("Server is running on port on 5000");
+});
 
-app.listen(5000,()=>{
-    console.log("Server is running on port on 5000")
-})
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
-app.use("/api/user", userRoute)
-
-
-app.get('/',(req,res)=>{
-    res.json({
-        message:"Welcome to Auth API"
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Auth API",
+  });
+});
