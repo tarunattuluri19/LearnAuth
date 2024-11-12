2130e0bd830617da4b733defabd50fd3cf4e8f5f
tarunattuluri19 <TARUNATTULURI19@GMAIL.COM>
2024-11-12 11:24:12 +0530

    empty

--- api/server.js
+++ api/server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(5000, () => {
-  console.log('Server listening on port 3000');
+  console.log('Server listening on port 5000');
 });
 
 app.use('/api/user', userRoutes);
